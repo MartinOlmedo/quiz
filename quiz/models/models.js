379add0a19a3bcc854c55a0619453a5260bfb00c
MartinOlmedo <martinolmedo985@gmail.com>
2015-07-07 23:50:59 +0200

    Despliegue DB en Heroku

--- quiz/models/models.js
+++ quiz/models/models.js
@@ -1,16 +1,42 @@
 var path = require('path');
-// console.log('************-PATH--> ' + path);
+
+// postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLITE DATABASE_URL  = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_NAME = (URL[6] || null);
+var user = (URL[2] || null);
+var pwd = (URL[3] || null);
+var protocol = (URL[1] || null);
+var dialect = (URL[1] || null);
+var port = (URL[5] || null);
+var host = (URL[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+
 // carga modelo ORM
 var Sequelize = require('sequelize');
-// console.log('************-Sequelize--> ' + Sequelize);
+
+
 // usa bbdd sqlite
-var sequelize = new Sequelize(null,null,null,
-	{dialect: "sqlite",storage:"quiz.sqlite"});
-// console.log('************-sequelize--> ' + sequelize);
+//var sequelize = new Sequelize(null,null,null,
+//	{dialect: "sqlite",storage:"quiz.sqlite"});
+
+var sequelize = new Sequelize(DB_NAME,user,pwd,
+	{
+		dialect: protocol,
+		protocol: protocol,
+		port: port,
+		host: host,
+		storage: storage,
+		omitNull: true
+	});
+
 
 // importa la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-// console.log('************-Quiz--> ' + Quiz);
+
+
 // exporta definicion de la tabla Quiz
 exports.Quiz=Quiz;
 
